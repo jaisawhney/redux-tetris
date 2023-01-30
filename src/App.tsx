872dae7608c9ff4950a7a5337ce5a3115e0fb7c4
Jai Sawhney <jai.sawhney1@gmail.com>
2023-01-30 11:29:35 -0800

    Add scoreboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import './App.css';
 import GridSquare from './components/GridSquare'
 import GridBoard from './components/GridBoard';
+import NextBlock from "./components/NextBlock";
+import ScoreBoard from "./components/ScoreBoard";
 
 function App() {
-  return (
-      <div className='App'>
-        <header className='App-header'>
-          <h1 className='App-title'>Tetris</h1>
-        </header>
-          <GridBoard />
-      </div>
-  );
+    return (
+        <div className='App'>
+            <header className='App-header'>
+                <h1 className='App-title'>Tetris</h1>
+            </header>
+            <GridBoard/>
+            <NextBlock/>
+            <ScoreBoard/>
+        </div>
+    );
 }
 
 export default App;

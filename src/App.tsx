c3bf80690ab2b7712b25162ee2c3b4c3fa6c0025
Jai Sawhney <jai.sawhney1@gmail.com>
2023-01-30 10:58:30 -0800

    Add grid board

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import './App.css';
 import GridSquare from './components/GridSquare'
+import GridBoard from './components/GridBoard';
 
 function App() {
   return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Tetris</h1>
+      <div className='App'>
+        <header className='App-header'>
+          <h1 className='App-title'>Tetris</h1>
         </header>
-        <GridSquare color="2"/>
+          <GridBoard />
       </div>
   );
 }

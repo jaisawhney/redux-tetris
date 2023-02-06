4e53ccfc76411ba8fcfc4443de4296789888541b
Jai Sawhney <jai.sawhney1@gmail.com>
2023-02-06 11:17:33 -0800

    Grid board connection

--- src/components/GridBoard.tsx
+++ src/components/GridBoard.tsx
@@ -1,14 +1,36 @@
 import GridSquare from './GridSquare';
-import { ReactElement } from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from '../app/store';
+import { shapes } from '../utils';
 
 export default function GridBoard() {
-    const grid: Array<ReactElement[]> = [];
-    for (let row = 0; row < 18; row++) {
-        grid.push([]);
-        for (let col = 0; col < 10; col++) {
-            grid[row].push(<GridSquare key={`${col}${row}`} color="0" />);
-        }
-    }
+    const { grid, shape, rotation, x, y } = useSelector(
+        (state: RootState) => state.game
+    );
 
-    return <div className="grid-board">{grid}</div>;
+    const block = shapes[shape][rotation];
+    const blockColor = shape;
+
+    const gridSquares = grid.map((rowArray, row) => {
+        return rowArray.map((square, col) => {
+            const blockX = col - x;
+            const blockY = row - y;
+            let color = square;
+
+            if (
+                blockX >= 0 &&
+                blockX < block.length &&
+                blockY >= 0 &&
+                blockY < block.length
+            ) {
+                color = block[blockY][blockX] === 0 ? color : blockColor;
+            }
+            // Generate a unique key for every block
+            const k = row * grid[0].length + col;
+
+            // Generate a grid square
+            return <GridSquare key={k} color={color.toString()} />;
+        });
+    });
+    return <div className="grid-board">{gridSquares}</div>;
 }

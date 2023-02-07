90dc90e2c0364b89af3b2baa8df8de7839e604da
Jai Sawhney <jai.sawhney1@gmail.com>
2023-02-06 16:15:42 -0800

    Controls connection

--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -1,40 +1,50 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState } from '../app/store';
+import { moveDown, moveLeft, moveRight, rotate } from '../features/gameSlice';
 
 export default function Controls() {
+    const isRunning = useSelector((state: RootState) => state.game.isRunning);
+    const dispatch = useDispatch();
+
     return (
         <div className="controls">
             {/* left */}
             <button
+                disabled={!isRunning}
                 className="control-button"
-                onClick={(e) => {
-                    // ...
+                onClick={() => {
+                    dispatch(moveLeft());
                 }}>
                 Left
             </button>
 
             {/* right */}
             <button
+                disabled={!isRunning}
                 className="control-button"
-                onClick={(e) => {
-                    // ...
+                onClick={() => {
+                    dispatch(moveRight());
                 }}>
                 Right
             </button>
 
             {/* rotate */}
             <button
+                disabled={!isRunning}
                 className="control-button"
-                onClick={(e) => {
-                    // ...
+                onClick={() => {
+                    dispatch(rotate());
                 }}>
                 Rotate
             </button>
 
             {/* down */}
             <button
+                disabled={!isRunning}
                 className="control-button"
-                onClick={(e) => {
-                    // ...
+                onClick={() => {
+                    dispatch(moveDown());
                 }}>
                 Down
             </button>

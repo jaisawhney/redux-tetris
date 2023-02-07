a5494af76467e9e7382196bc23b2a5c29216822f
Jai Sawhney <jai.sawhney1@gmail.com>
2023-02-06 16:39:08 -0800

    Message popup connection

--- src/components/MessagePopup.tsx
+++ src/components/MessagePopup.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from '../app/store';
 
 export default function MessagePopup() {
+    const { isRunning, gameOver } = useSelector(
+        (state: RootState) => state.game
+    );
+
+    let gameStatus = '';
+    let isHidden = true;
+    if (gameOver) {
+        isHidden = false;
+        gameStatus = 'Game Over';
+    } else if (!isRunning) {
+        isHidden = false;
+        gameStatus = 'Paused';
+    }
+
     return (
-        <div className="message-popup">
-            <h1>Message Title</h1>
-            <p>Message info...</p>
+        <div className={'message-popup' + (isHidden ? ' hidden' : '')}>
+            <h1>{gameStatus}</h1>
         </div>
     );
 }
